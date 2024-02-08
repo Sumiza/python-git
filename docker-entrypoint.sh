714a25d88352f61bbf9fa364cf495b566a5e91b3
Sumiza <64884509+Sumiza@users.noreply.github.com>
2024-02-07 22:33:04 -0500

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ -n "$PIP_INSTALL" ]
+then
+    for i in $PIP_INSTALL
+        do
+            pip install "$i"
+        done
+fi
+
+if [ -n "$TOKEN" ]; then
+        tok="$TOKEN"@
+else
+        tok=""
+fi
+
+url=https://"$tok"github.com/"$USER"/"$REPO"
+
+apk add git
+git clone --depth 1 --quiet "$url" || exit 1
+
+cd $REPO || exit 1
+
+exec "$@"
