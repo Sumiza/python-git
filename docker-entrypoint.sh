cce86ea249967e33086e8dde8c2f52499153ee37
Sumiza <64884509+Sumiza@users.noreply.github.com>
2024-02-09 19:27:10 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-if [ -n "$PIP_INSTALL" ]
-then
+if [ -n "$PIP_INSTALL" ]; then
     for i in $PIP_INSTALL
         do
             pip install "$i"
         done
+fi
+
+if [ -n "$INSTALL" ]; then
+        if command -v apt > /dev/null; then
+                apt update && apt install "$INSTALL" -y
+        elif command -v apk > /dev/null; then
+                apk add "$INSTALL"
+        fi
 fi
 
 if [ -n "$TOKEN" ]; then
